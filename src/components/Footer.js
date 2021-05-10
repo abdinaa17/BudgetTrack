7cc63b7df5285c9f044aea26f2746eaa9931556a
abdinaa17 <70495316+abdinaa17@users.noreply.github.com>
2021-05-10 11:37:37 -0500

    a fix to the twitter link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className='footer'>
 
             <h3>Made by <span>Nasir</span></h3>
-            <p>Follow Me On <a href="https://twitter.com/a_nasri8" target="_blank" rel="noopener" noreferrer><span>Twitter</span></a></p>
+            <p>Follow Me On <a href="https://twitter.com/a_nasri8" target="_blank" rel="noopener noreferrer" ><span>Twitter</span></a></p>
             
         </footer>
     )
